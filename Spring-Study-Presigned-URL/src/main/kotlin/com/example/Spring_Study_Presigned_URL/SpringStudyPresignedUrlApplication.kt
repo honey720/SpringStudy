83096a9d8efd846a088f15254156d87b120b2d55
honey720 <honey720@naver.com>
2024-11-27 21:11:03 +0900

    first commit

---
+++ Spring-Study-Presigned-URL/src/main/kotlin/com/example/Spring_Study_Presigned_URL/SpringStudyPresignedUrlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Spring_Study_Presigned_URL
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringStudyPresignedUrlApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringStudyPresignedUrlApplication>(*args)
+}
---
+++ Spring-Study-Presigned-URL/src/test/kotlin/com/example/Spring_Study_Presigned_URL/SpringStudyPresignedUrlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Spring_Study_Presigned_URL
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringStudyPresignedUrlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
